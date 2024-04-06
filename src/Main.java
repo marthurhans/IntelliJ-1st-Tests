1a6a025ae2b3babd2f94ddaf896daac985922d9f
marthurhans <marthurhans@gmail.com>
2024-04-06 16:18:21 -0400

    The 3nd version of this Test - 2024-0406

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
-            System.out.println("This is a Test");
+        for (int i = 0; i < 3; i++) {
+            System.out.println("version 1");
+        }
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Version 2");
         }
     }
 }
